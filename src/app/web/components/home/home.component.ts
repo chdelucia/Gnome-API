07621bcbc70fd460086817051c0d508a1bb71bed
Christian <c.heredia87@gmail.com>
2017-08-17 22:38:08 +0200

    added filter by profession

--- src/app/web/components/home/home.component.ts
+++ src/app/web/components/home/home.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild }       from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { BrastlewarkService }                     from './../../shared/services/brastlewark.service';
-import { PagerService }                       from './../../shared/services/pager.service';
-import { Observable }                         from 'rxjs/Observable';
-import { environment }                        from './../../../../environments/environment';
-import { Brastlewark }                      from './../../shared/services/brastlewark';
+import { BrastlewarkService } from './../../shared/services/brastlewark.service';
+import { PagerService } from './../../shared/services/pager.service';
+import { Observable } from 'rxjs/Observable';
+import { environment } from './../../../../environments/environment';
+import { Brastlewark } from './../../shared/services/brastlewark';
 
 @Component({
   selector: 'app-home',
@@ -13,66 +13,72 @@ import { Brastlewark }                      from './../../shared/services/brastl
 })
 export class HomeComponent implements OnInit {
 
-  inhabitants : Brastlewark[] = [];
-  pagedInhabitants : Brastlewark[] = [];
+  inhabitants: Brastlewark[] = [];
+  pagedInhabitants: Brastlewark[] = [];
   jobs = [];
-  actualPage : number;
+  actualPage: number;
   pager: any = {};
 
-  constructor( private brastlewark: BrastlewarkService,private pagerService: PagerService) {
+  constructor(private brastlewark: BrastlewarkService, private pagerService: PagerService) {
     this.actualPage = 1;
-   }
-    
+  }
+
   ngOnInit() {
     this.recoverDataFromStorage();
     this.getInhabitants();
   }
 
-  getInhabitants(){  
-      this.brastlewark.getInhabitants().subscribe( 
-        result => {
-          console.log(result);
-          this.inhabitants = result.Brastlewark;
-          this.setPage(this.actualPage);
-          this.listJobs();
+  getInhabitants() {
+    this.brastlewark.getInhabitants().subscribe(
+      result => {
+        console.log(result);
+        this.inhabitants = result.Brastlewark;
+        this.setPage(this.actualPage);
+        this.listJobs();
       })
   }
 
-
-  listJobs(){
+  listJobs() {
     this.inhabitants.map(item => {
       item.professions.map(job => {
-        if (!this.jobs.includes(job.trim())){ 
+        if (!this.jobs.includes(job.trim())) {
           this.jobs.push(job.trim())
         }
       })
     })
-    debugger;
     this.jobs = this.jobs.sort();
   }
 
-  
+  filterByProfession(value: string) {
+    this.pagedInhabitants = [];
+    this.inhabitants.map(item => {
+      item.professions.map(job => {
+        if (job.trim() === value) {
+          this.pagedInhabitants.push(item);
+        }
+      })
+    })
+  }
 
   /*
   * Look for a previous  inhabitans if exist in Storage
   */
-  recoverDataFromStorage(){
-      if (localStorage.getItem('Brastlewark') !== null) {
-        this.inhabitants = this.brastlewark.getLocalStorage().Brastlewark;
-      }
+  recoverDataFromStorage() {
+    if (localStorage.getItem('Brastlewark') !== null) {
+      this.inhabitants = this.brastlewark.getLocalStorage().Brastlewark;
+    }
   }
 
   setPage(page: number) {
     if (page < 1 || page > this.pager.totalPages) {
-        return;
+      return;
     }
-
     // get pager object from service
     this.pager = this.pagerService.getPager(this.inhabitants.length, page);
-    
+
     // get current page of items
     this.pagedInhabitants = this.inhabitants.slice(this.pager.startIndex, this.pager.endIndex + 1);
-}
-  
+  }
+
 
 }
