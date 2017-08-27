daa87ba08e513d9b4f716ab843da4dbe2fde93c5
Christian <c.heredia87@gmail.com>
2017-08-27 19:33:02 +0200

    e2e test updates

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { MediacoachportalPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('mediacoachportal App', () => {
   let page: MediacoachportalPage;
@@ -14,7 +15,21 @@ describe('mediacoachportal App', () => {
 
   it('should display 12 inhabitants', () => {
     page.navigateTo();
+    browser.sleep(2000);
     expect(page.getInhabitants().count()).toEqual(12);
+  });
+  
+  it('should not exist yet', () => {
+    page.navigateTo();
+    browser.sleep(2000);
+    expect(page.getInhabitantsFilteredByname().count()).toBe(0);
+  })
+
+  it('should display filtered names', () => {
+    page.navigateTo();
+    page.getSearchInput().sendKeys('quickwhis');
+    browser.sleep(2000);
+    expect(page.getInhabitantsFilteredByname().count()).toEqual(6);
   });
 
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -15,4 +15,12 @@ export class MediacoachportalPage {
   getInhabitants(){
     return element.all(by.className('portfolio-item'));
   }
+
+  getInhabitantsFilteredByname(){
+    return element.all(by.className('names-filtered__item'));
+  }
+
+  getSearchInput(){
+    return element(by.className('form-control'));
+  }
 }
